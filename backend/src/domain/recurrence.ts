661fc0c17dfc0d38a7fb5eb4f426f4cc200262b7
Emilia <emiliama2006@gmail.com>
2026-08-28 23:21:29 -0400

    starting recurrence logic

--- backend/src/domain/recurrence.ts
+++ backend/src/domain/recurrence.ts
@@ -1,12 +1,13 @@
 export interface recurrence {
     frequency: string; //daily, every week, every month, every year
-    weekday?: number; //if the recurrence depends on weekday instead of days, pick which weekday
+    weekday?: number[] //if the recurrence depends on weekday instead of days, pick which weekday
 
-    days?: number; //nº of days after the beginning of the frequency unit: 0 days would be sunday for week, 1st of the month etc.
+    days?: number[] //nº of days after the beginning of the frequency unit: 0 days would be sunday for week, 1st of the month etc.
     skipInterval?: number; //if monthly: (monthly + skipinterval 1: every other month)  if weekly + weekday + skipint 2, every 3rd {weekday}
-    position?: number; //if positive: selects {position}th occurence of weekday. if negative, go backwards and select that occurence
+    position?: number[] //if positive: selects {position}th occurence of weekday. if negative, go backwards and select that occurence
+    months?: number[] //if yearly, might need specific months: every march and may
 
-    endDate?: string: //YYYY-MM-DD. all repetitions of this event end at or before this date
+    endDate?: string; //YYYY-MM-DD. all repetitions of this event end at or before this date
 }
 
 /*
@@ -14,9 +15,6 @@ examples:
 • every other day:
     frequency: daily
     skipInterval: 1
-• every thursday
-    frequency: weekly
-    weekday: thursday
 • every other thursday:
     frequency: weekly
     weekday: thursday
