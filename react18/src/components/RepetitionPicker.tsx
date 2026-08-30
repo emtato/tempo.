25379bdb6e95b449ec0ddb45a9481a5a74b8b686
Emilia <emiliama2006@gmail.com>
2026-08-29 21:48:35 -0400

    starting recurrence popup

--- react18/src/components/RepetitionPicker.tsx
+++ react18/src/components/RepetitionPicker.tsx
@@ -4,10 +4,20 @@ import {recurrence} from "../../../backend/src/domain/recurrence";
 interface RepetitionPickerProps {
     onChange: (date: Date, startOrEnd: "start" | "end") => void; //callback send recurrence info back
     recurrenceSelection?: recurrence //will be modified by the popup and sent back when done to onchange
+    options: string[]; //options for simple repetition (based on date clicked, intelligently offer suggestions)
 }
 
+function optionSelected(option: string) {
 
-export default function RepetitionPicker() {
+}
+
+function formatOptionsToText(option: string): string {
+
+
+    return ""
+}
+
+export default function RepetitionPicker(props: RepetitionPickerProps) {
     const [isOpen, setIsOpen] = useState(false)
 
 
@@ -20,8 +30,18 @@ export default function RepetitionPicker() {
             <button className="repeat-button" type="button" onClick={() => {
                 setIsOpen(true)
             }}>Does not repeat</button>
+            {/* TODO: connect repetition status of the event to what displas on the button*/}
 
-            {isOpen && (<span> opened</span>)}
+            {isOpen && (<span className="repetition-menu">
+                        {props.options.map((option) => (
+                            <button
+                                type="button"
+                                //key={date.getTime()}
+                                className="date-option-button"
+                                onClick={() => optionSelected(option)}>
+                                {formatOptionsToText(option)}</button>
+                        ))}
+            </span>)}
                 </span>
     );
 }
